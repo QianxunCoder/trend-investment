bc236617111f533e335c60914a674b42f8d3d59d
QingKong <1031556593@qq.com>
2024-02-24 09:24:09 +0800

    初始化第三方数据服务

---
+++ third-part-index-data-project/src/main/java/com/github/codeqingkong/ThirdPartIndexDataApplication.java
@@ -0,0 +1,13 @@
+package com.github.codeqingkong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ThirdPartIndexDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdPartIndexDataApplication.class, args);
+    }
+}
